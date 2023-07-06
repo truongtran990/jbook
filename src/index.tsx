4e25b182fa269e2b04a8b346b6adcd82047b9cb8
VanTruongUIT <tvtruong.uit@gmail.com>
2023-07-07 00:28:23 +0700

    initialize esbuild

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,26 @@
-import { useState } from "react";
+import * as esbuild from "esbuild-wasm";
+import { useState, useEffect } from "react";
 
 import { createRoot } from "react-dom/client";
 
 const App = () => {
   const [inputRaw, setInputRaw] = useState("");
   const [code, setCode] = useState("");
+
+  // initialize esbuild
+  const startService = async () => {
+    // return the service object that actually bundle, transform and transport.
+    const service = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+
+    console.log(service);
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
 
   const handleSubmitInput = () => {
     console.log(inputRaw);

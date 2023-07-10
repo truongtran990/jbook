c49bb456beaf55789a060c3e9741848bc17a923b
VanTruongUIT <tvtruong.uit@gmail.com>
2023-07-11 01:26:36 +0700

    generate unpkg package url

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -32,12 +32,17 @@ export const unpkgPathPlugin = () => {
             path: args.path,
             namespace: "a",
           };
-        } else if (args.path === "tiny-test-pkg") {
+        }
+        return {
+          namespace: "a",
+          path: `https://unpkg.com/${args.path}`,
+        };
+        /* else if (args.path === "tiny-test-pkg") {
           return {
             path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
             namespace: "a",
           };
-        }
+        } */
       });
 
       /* 

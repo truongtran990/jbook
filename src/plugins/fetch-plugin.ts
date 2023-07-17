206ce49ad156d17967a6b04425761a18cb2071c4
VanTruongUIT <tvtruong.uit@gmail.com>
2023-07-17 23:14:32 +0700

    separate load file

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -11,28 +11,27 @@ export const fetchPlugin = (inputCode: string) => {
   return {
     name: "fetch-plugin",
     setup(build: esbuild.PluginBuild) {
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log("onLoad", args);
+      // exactly load index.js file
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        console.log("onLoad");
+        return {
+          loader: "jsx",
+          contents: inputCode,
+        };
+      });
 
-        if (args.path === "index.js") {
-          return {
-            loader: "jsx",
-            contents: inputCode,
-          };
+      // load file which is ended with .css
+      build.onLoad({ filter: /\.css$/ }, async (args: any) => {
+        console.log("onLoad", args);
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+
+        if (cachedResult) {
+          return cachedResult;
         }
 
-        // const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
-        //   args.path
-        // );
-
-        // if (cachedResult) {
-        //   return cachedResult;
-        // }
-
         const { data, request } = await axios.get(args.path);
-
-        const fileType = args.path.match(/.css$/) ? "css" : "jsx";
-        console.log("fileType: ", fileType);
 
         const escaped = data
           // remove new line by ""
@@ -42,18 +41,41 @@ export const fetchPlugin = (inputCode: string) => {
           // replace ' => \'
           .replace(/'/g, "\\'");
 
-        const contents =
-          fileType === "css"
-            ? `
+        const contents = `
           const style = document.createElement('style');
           style.innerText = '${escaped}';
           document.head.appendChild(style);
-        `
-            : data;
+        `;
 
         const result: esbuild.OnLoadResult = {
           loader: "jsx",
           contents,
+          // we get the directory to the main file of library like this: https://unpkg.com/nested-test-pkg.com@17.0.1/src/index.js ==> /nested-test-pkg@1.0.0/src/
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
+
+        // set the fetched data into localforage
+        await fileCache.setItem(args.path, result);
+
+        return result;
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        console.log("onLoad", args);
+
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+
+        if (cachedResult) {
+          return cachedResult;
+        }
+
+        const { data, request } = await axios.get(args.path);
+
+        const result: esbuild.OnLoadResult = {
+          loader: "jsx",
+          contents: data,
           // we get the directory to the main file of library like this: https://unpkg.com/nested-test-pkg.com@17.0.1/src/index.js ==> /nested-test-pkg@1.0.0/src/
           resolveDir: new URL("./", request.responseURL).pathname,
         };

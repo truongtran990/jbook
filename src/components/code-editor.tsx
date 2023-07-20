c6259f17a01365ea37f875f9fda25b9937598762
VanTruongUIT <tvtruong.uit@gmail.com>
2023-07-20 23:47:48 +0700

    format the code

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,4 +1,7 @@
-import MonacoEditor from "@monaco-editor/react";
+import { useRef } from "react";
+import MonacoEditor, { EditorDidMount } from "@monaco-editor/react";
+import prettier from "prettier";
+import parser from "prettier/parser-babel";
 
 interface CodeEditorProps {
   initialValue: string;
@@ -6,30 +9,57 @@ interface CodeEditorProps {
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
-  const handleEditorDidMount = (getValue: () => string, monacoEditor: any) => {
+  const editorRef = useRef<any>();
+
+  const handleEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+    editorRef.current = monacoEditor;
+
     monacoEditor.onDidChangeModelContent(() => {
       onChange(getValue());
     });
+
+    monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
+  };
+
+  const handleFormat = () => {
+    // get current value from editor
+    console.log(editorRef.current);
+    const unformatted = editorRef.current.getModel().getValue();
+
+    // format that value
+    const formatted = prettier.format(unformatted, {
+      parser: "babel",
+      plugins: [parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true,
+    });
+
+    // set the formatted value back in the editor
+    editorRef.current.setValue(formatted);
   };
 
   return (
-    <MonacoEditor
-      options={{
-        wordWrap: "on",
-        minimap: { enabled: false },
-        showUnused: false,
-        folding: false,
-        lineNumbersMinChars: 3,
-        fontSize: 16,
-        scrollBeyondLastLine: false,
-        automaticLayout: true,
-      }}
-      language="javascript"
-      theme="dark"
-      height="500px"
-      value={initialValue}
-      editorDidMount={handleEditorDidMount}
-    />
+    <div>
+      <button onClick={handleFormat}>Format</button>
+      <MonacoEditor
+        options={{
+          wordWrap: "on",
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+        }}
+        language="javascript"
+        theme="dark"
+        height="500px"
+        value={initialValue}
+        editorDidMount={handleEditorDidMount}
+      />
+    </div>
   );
 };
 

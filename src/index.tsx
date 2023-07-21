9367d181a272ec9a8f8882c1e178b066794db515
VanTruongUIT <tvtruong.uit@gmail.com>
2023-07-21 23:24:49 +0700

    extract logic for bundling

--- src/index.tsx
+++ src/index.tsx
@@ -7,46 +7,17 @@ import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 import { fetchPlugin } from "./plugins/fetch-plugin";
 import CodeEditor from "./components/code-editor";
 import Preview from "./components/preview";
+import bundle from "./bundler";
 
 const App = () => {
   const [rawInput, setRawInput] = useState("");
   const [code, setCode] = useState("");
 
-  const ref = useRef<any>();
-
-  // initialize esbuild
-  const startService = async () => {
-    // return the service object that actually bundle, transform and transport.
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
-    });
-  };
-
-  useEffect(() => {
-    startService();
-  }, []);
-
   const handleSubmitInput = async () => {
-    if (!ref.current) {
-      return;
-    }
-
     // transpile the rawInput to js code
-    const result = await ref.current.build({
-      // index.js will be the first file of bundling process
-      entryPoints: ["index.js"],
-      bundle: true,
-      write: false,
-      // plugins go from left to write onResolve -> onLoad
-      plugins: [unpkgPathPlugin(), fetchPlugin(rawInput)],
-      define: {
-        "process.env.NODE_ENV": "'production'",
-        global: "window",
-      },
-    });
+    const output = await bundle(rawInput);
 
-    setCode(result.outputFiles[0].text);
+    setCode(output);
   };
 
   return (

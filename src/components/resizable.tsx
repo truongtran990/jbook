4f4c9fd387cce92c403af558358744552adba44b
VanTruongUIT <tvtruong.uit@gmail.com>
2023-07-23 09:51:16 +0700

    adding scc style for resizable component

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,12 +1,18 @@
 import { ResizableBox } from "react-resizable";
 
+import "./resizable.css";
+
 interface ResizableProps {
   direction: "horizontal" | "vertical";
   children?: React.ReactNode;
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
-  return <div>{children}</div>;
+  return (
+    <ResizableBox height={300} width={300} resizeHandles={["s"]}>
+      {children}
+    </ResizableBox>
+  );
 };
 
 export default Resizable;

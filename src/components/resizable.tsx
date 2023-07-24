46d1408d1e838e73ed5c385ecf641f6c39af8764
VanTruongUIT <tvtruong.uit@gmail.com>
2023-07-24 22:31:05 +0700

    improve performance when dragging

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { ResizableBox, ResizableBoxProps } from "react-resizable";
 
 import "./resizable.css";
@@ -9,23 +10,44 @@ interface ResizableProps {
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   let resizableProps: ResizableBoxProps;
+  const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    let timer: any;
+    const listener = () => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(() => {
+        console.log(window.innerWidth, window.innerHeight);
+        setInnerHeight(window.innerHeight);
+        setInnerWidth(window.innerWidth);
+      }, 100);
+    };
+    window.addEventListener("resize", listener);
+
+    return () => {
+      window.removeEventListener("resize", listener);
+    };
+  }, []);
 
   if (direction === "horizontal") {
     resizableProps = {
       className: "resize-horizontal",
       height: Infinity,
-      width: window.innerHeight * 0.8,
+      width: innerHeight * 0.8,
       resizeHandles: ["e"],
-      maxConstraints: [window.innerHeight * 0.8, Infinity],
-      minConstraints: [window.innerHeight * 0.2, Infinity],
+      maxConstraints: [innerHeight * 0.8, Infinity],
+      minConstraints: [innerHeight * 0.2, Infinity],
     };
   } else {
     resizableProps = {
       height: 300,
       width: Infinity,
       resizeHandles: ["s"],
-      maxConstraints: [Infinity, window.innerHeight * 0.9],
-      minConstraints: [Infinity, window.innerHeight * 0.1],
+      maxConstraints: [Infinity, innerHeight * 0.9],
+      minConstraints: [Infinity, innerHeight * 0.1],
     };
   }
   return <ResizableBox {...resizableProps}>{children}</ResizableBox>;

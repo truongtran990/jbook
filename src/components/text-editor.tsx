5b605841f07bd634c9dd508eb137f9ebd4005c32
VanTruongUIT <tvtruong.uit@gmail.com>
2023-08-02 22:51:47 +0700

    switch between viewing and editing modes

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,8 +1,31 @@
+import { useState, useEffect } from "react";
 import MDEditor from "@uiw/react-md-editor";
 
 const TextEditor: React.FC = () => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  useEffect(() => {
+    const listener = () => {
+      setIsEditing(false);
+    };
+
+    // Whenever user click into the MDEditor -> switch to view mode
+    document.addEventListener("click", listener, { capture: true });
+
+    return () => {
+      document.removeEventListener("click", listener, { capture: true });
+    };
+  }, []);
+
+  if (isEditing) {
+    return (
+      <div>
+        <MDEditor />
+      </div>
+    );
+  }
   return (
-    <div>
+    <div onClick={() => setIsEditing(true)}>
       <MDEditor.Markdown source={"# Header"} />
     </div>
   );

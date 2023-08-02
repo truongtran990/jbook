92df37ce191dabcd92aeb11547851fffa5bc9c6c
VanTruongUIT <tvtruong.uit@gmail.com>
2023-08-02 23:01:51 +0700

    handle click outside editor to switch view mode

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,11 +1,21 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import MDEditor from "@uiw/react-md-editor";
 
 const TextEditor: React.FC = () => {
   const [isEditing, setIsEditing] = useState(false);
+  const mdEditorRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    const listener = () => {
+    const listener = (event: MouseEvent) => {
+      if (
+        mdEditorRef.current &&
+        event.target &&
+        mdEditorRef.current.contains(event.target as Node)
+      ) {
+        console.log("element clicked is inside the editor");
+        return;
+      }
+      console.log("element clicked is not inside the editor");
       setIsEditing(false);
     };
 
@@ -19,7 +29,7 @@ const TextEditor: React.FC = () => {
 
   if (isEditing) {
     return (
-      <div>
+      <div ref={mdEditorRef}>
         <MDEditor />
       </div>
     );

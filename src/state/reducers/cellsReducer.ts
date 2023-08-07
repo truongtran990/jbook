d76a560384ffacd3b716ad99f141a98957a27d69
VanTruongUIT <tvtruong.uit@gmail.com>
2023-08-07 22:25:17 +0700

    define the reducer for cells

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,1 +1,31 @@
+import { ActionType } from "../action-types";
+import { Action } from "../actions";
+import { Cell } from "../cell";
 
+// define the interface for state
+interface CellsState {
+  loading: boolean;
+  error: string | null;
+  order: string[];
+  data: {
+    [key: string]: Cell;
+  };
+}
+
+// initial state varialbe
+const initialState: CellsState = {
+  loading: false,
+  error: null,
+  order: [],
+  data: {},
+};
+
+// define the reducer object, that will receive the current state, and action and then return the new state
+const reducer = (
+  state: CellsState = initialState,
+  action: Action
+): CellsState => {
+  return state;
+};
+
+export default reducer;

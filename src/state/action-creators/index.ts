1e11ff785a4e28f6f559c1abef54dbf607a77d3e
VanTruongUIT <tvtruong.uit@gmail.com>
2023-08-08 23:27:53 +0700

    action creators return type annotation

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,1 +1,14 @@
+import { ActionType } from "../action-types";
+import {
+  Action,
+  MoveCellAction,
+  DeleteCellAction,
+  InsertCellBeforeAction,
+  UpdateCellAction,
+} from "../actions";
 
+// Here are action creator functions, which return the perspective action
+export const updateCell = (): UpdateCellAction => {};
+export const deleteCell = (): DeleteCellAction => {};
+export const moveCell = (): MoveCellAction => {};
+export const insertCellBefore = (): InsertCellBeforeAction => {};

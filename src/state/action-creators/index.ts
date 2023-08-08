e322b9a9b555a76ad6187e40beb54e3de5049a6a
VanTruongUIT <tvtruong.uit@gmail.com>
2023-08-08 23:53:40 +0700

    implement the action creators

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -6,9 +6,45 @@ import {
   InsertCellBeforeAction,
   UpdateCellAction,
 } from "../actions";
+import { CellTypes } from "../cell";
 
 // Here are action creator functions, which return the perspective action
-export const updateCell = (): UpdateCellAction => {};
-export const deleteCell = (): DeleteCellAction => {};
-export const moveCell = (): MoveCellAction => {};
-export const insertCellBefore = (): InsertCellBeforeAction => {};
+export const updateCell = (id: string, content: string): UpdateCellAction => {
+  return {
+    type: ActionType.UPDATE_CELL,
+    payload: {
+      id,
+      content,
+    },
+  };
+};
+export const deleteCell = (id: string): DeleteCellAction => {
+  return {
+    type: ActionType.DELETE_CELL,
+    payload: id,
+  };
+};
+export const moveCell = (
+  id: string,
+  direction: "up" | "down"
+): MoveCellAction => {
+  return {
+    type: ActionType.MOVE_CELL,
+    payload: {
+      id,
+      direction,
+    },
+  };
+};
+export const insertCellBefore = (
+  id: string,
+  type: CellTypes
+): InsertCellBeforeAction => {
+  return {
+    type: ActionType.INSERT_CELL_BEFORE,
+    payload: {
+      id,
+      type,
+    },
+  };
+};

c12c383a91ec65cbe5f5b906536e6325ebdc9133
VanTruongUIT <tvtruong.uit@gmail.com>
2023-08-16 21:42:06 +0700

    rendering list of cell

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,5 +1,17 @@
+import { useTypedSelector } from "../hooks/use-typed-selector";
+import CellListItem from "./cell-list-item";
+
 const CellList: React.FC = () => {
-  return <div>Cell List</div>;
+  //@ts-ignore
+  const cells = useTypedSelector(({ cells: { order, data } }) => {
+    return order.map((id: string) => data[id]);
+  });
+
+  const renderedCells = cells.map((cell) => (
+    <CellListItem key={cell.id} cell={cell} />
+  ));
+
+  return <ul>{renderedCells}</ul>;
 };
 
 export default CellList;

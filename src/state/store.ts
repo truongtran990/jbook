727cf8f8f2bb4b958dd939fb939174a4f520bdbd
VanTruongUIT <tvtruong.uit@gmail.com>
2023-08-16 22:25:06 +0700

    render cell type based on type

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,6 +1,22 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 
+import { ActionType } from "./action-types";
 import reducers from "./reducers";
 
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: "001",
+    type: "code",
+  },
+});
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: "002",
+    type: "text",
+  },
+});

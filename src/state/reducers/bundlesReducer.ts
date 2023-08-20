5946917ba0384f94fb7f113b9ef4ecc4de79e7d2
VanTruongUIT <tvtruong.uit@gmail.com>
2023-08-20 13:31:45 +0700

    define the reducer for bundlesState

--- src/state/reducers/bundlesReducer.ts
+++ src/state/reducers/bundlesReducer.ts
@@ -1,2 +1,27 @@
-const nothing = "";
-export default nothing;
+import { produce } from "immer";
+import { ActionType } from "../action-types";
+import { Action } from "../actions";
+
+interface BundlesState {
+  [key: string]: {
+    loading: boolean;
+    code: string;
+    err: string;
+  };
+}
+const initialBundlesState: BundlesState = {};
+
+const reducer = produce(
+  (state: BundlesState = initialBundlesState, action: Action): BundlesState => {
+    switch (action.type) {
+      case ActionType.BUNDLE_START:
+        return state;
+      case ActionType.BUNDLE_COMPLETE:
+        return state;
+      default:
+        return state;
+    }
+  }
+);
+
+export default reducer;

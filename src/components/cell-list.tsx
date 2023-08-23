6334701a3f87315b6daaea2ec46eb5609f0eb852
VanTruongUIT <tvtruong.uit@gmail.com>
2023-08-23 23:04:55 +0700

    beauty cell list by adding margin left and right

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
 
+import "./cell-list.css";
 import { useTypedSelector } from "../hooks/use-typed-selector";
 import CellListItem from "./cell-list-item";
 import AddCell from "./add-cell";
@@ -18,7 +19,7 @@ const CellList: React.FC = () => {
   ));
 
   return (
-    <div>
+    <div className="cell-list">
       <AddCell forceVisible={cells.length === 0} previousCellId={null} />
       {renderedCells}
     </div>

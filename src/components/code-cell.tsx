2b082afd075628afa10db5d52d0a34bc907e2e5e
VanTruongUIT <tvtruong.uit@gmail.com>
2023-08-23 22:59:14 +0700

    extracting logic to new custom hook is call useCumulativeCode

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -8,6 +8,7 @@ import Resizable from "./resizable";
 import { Cell } from "../state";
 import { useActions } from "../hooks/use-actions";
 import { useTypedSelector } from "../hooks/use-typed-selector";
+import { useCumulativeCode } from "../hooks/use-cumulative-code";
 
 interface CodeCellProps {
   cell: Cell;
@@ -17,62 +18,18 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const { updateCell, createBundle } = useActions();
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
 
-  // get all the code from the current cell and the previous cells
-  const cumulativeCode = useTypedSelector((state) => {
-    // @ts-ignore
-    const { data, order } = state.cells;
-    const orderedCells = order.map((id: string) => data[id]);
-
-    const showFunc = `
-      import _React from 'react';
-      import _ReactDOM from 'react-dom';
-      var show = (value) => {
-        console.log('show is called')
-        const root = document.querySelector('#root');
-        if (typeof value === 'object') {
-          /*Check if the value if the jsx element or not*/
-          if (value.$$typeof && value.props) {
-            _ReactDOM.render(value, root)
-          } else {
-            root.innerHTML = JSON.stringify(value);
-          }
-        } else {
-          root.innerHTML = value;
-        }
-      }
-    `;
-    const showFuncNoOp = "var show = () => {}";
-
-    const cumulativeCode = [];
-
-    for (const c of orderedCells) {
-      if (c.type === "code") {
-        /* logic to check if the currentthe current cell is the cell to run the code */
-        if (c.id === cell.id) {
-          cumulativeCode.push(showFunc);
-        } else {
-          cumulativeCode.push(showFuncNoOp);
-        }
-        cumulativeCode.push(c.content);
-      }
-      if (c.id === cell.id) {
-        break;
-      }
-    }
-
-    return cumulativeCode;
-  });
+  const cumulativeCode = useCumulativeCode(cell.id);
 
   useEffect(() => {
     // for the first initial bundle
     if (!bundle) {
-      createBundle(cell.id, cumulativeCode.join("\n"));
+      createBundle(cell.id, cumulativeCode);
       return;
     }
 
     const timer = setTimeout(async () => {
       // transpile the rawInput to js code
-      createBundle(cell.id, cumulativeCode.join("\n"));
+      createBundle(cell.id, cumulativeCode);
     }, 750);
 
     // the return statement will be called automatically the next time useeffect is called
@@ -80,7 +37,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
       clearTimeout(timer);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cumulativeCode.join("\n"), cell.id, createBundle]);
+  }, [cumulativeCode, cell.id, createBundle]);
 
   return (
     <Resizable direction="vertical">

adce3dae366f467665884559cdbdd189d190458b
VanTruongUIT <tvtruong.uit@gmail.com>
2023-09-20 23:41:46 +0700

    create middleware to reduce no. call to update state

--- src/state/store.ts
+++ src/state/store.ts
@@ -3,8 +3,13 @@ import thunk from "redux-thunk";
 
 // import { ActionType } from "./action-types";
 import reducers from "./reducers";
+import { persistMiddleware } from "./middlewares/persist-middleware";
 
-export const store = createStore(reducers, {}, applyMiddleware(thunk));
+export const store = createStore(
+  reducers,
+  {},
+  applyMiddleware(persistMiddleware, thunk)
+);
 
 // store.dispatch({
 //   type: ActionType.INSERT_CELL_AFTER,
